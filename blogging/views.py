1a39b942b87e891029fd35dec710f60595790748
Sergey Alentyev <sergey.alentyev@apijet.com>
2022-11-25 13:06:37 -0800

    Lesson 8 assignment

--- blogging/views.py
+++ blogging/views.py
@@ -3,28 +3,13 @@ from django.http import Http404
 from blogging.models import Post
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.template import loader
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
-def detail_view(request, post_id):
-    published = Post.objects.exclude(published_date__exact=None)
-    try:
-        post = published.get(pk=post_id)
-    except Post.DoesNotExist:
-        raise Http404
-    context = {'post': post}
-    return render(request, 'blogging/detail.html', context)
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blogging/detail.html'
 
-def stub_view(request, *args, **kwargs):
-    body = "Stub View\n\n"
-    if args:
-        body += "Args:\n"
-        body += "\n".join(["\t%s" % a for a in args])
-    if kwargs:
-        body += "Kwargs:\n"
-        body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
-    return HttpResponse(body, content_type="text/plain")
-
-def list_view(request):
-    published = Post.objects.exclude(published_date__exact=None)
-    posts = published.order_by('-published_date')
-    context = {'posts': posts}
-    return render(request, 'blogging/list.html', context)    
+class PostListView(ListView):
+    queryset = Post.objects.exclude(published_date__exact=None).order_by('-published_date')
+    template_name = 'blogging/list.html'
