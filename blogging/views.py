9ee6ec504208928bc383401ec3770af4bbb635b6
Sergey Alentyev <sergey.alentyev@apijet.com>
2022-11-28 12:46:24 -0800

    Applied black.

--- blogging/views.py
+++ blogging/views.py
@@ -6,10 +6,14 @@ from django.template import loader
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 
+
 class PostDetailView(DetailView):
     model = Post
-    template_name = 'blogging/detail.html'
+    template_name = "blogging/detail.html"
+
 
 class PostListView(ListView):
-    queryset = Post.objects.exclude(published_date__exact=None).order_by('-published_date')
-    template_name = 'blogging/list.html'
+    queryset = Post.objects.exclude(published_date__exact=None).order_by(
+        "-published_date"
+    )
+    template_name = "blogging/list.html"
